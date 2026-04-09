66002017adaea88f3eaa214bdaffca3ed3d8a085
Edwin Cedeno <edwin.e.cedeno@gmail.com>
2026-04-09 15:43:43 -0500

    fix 4

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ app.use(express.json())
 app.use('auth',auth.userRoutes)
 app.use('/usuarios', userRoutes)
 
+app.use((err, req, res, next) => {
+    console.error(err)
+    res.status(500).json({
+      error: err.message || 'Internal Server Error'
+    })
+  })
+
 export default app
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,1 @@
-// src/routes/user.routes.js
-import express from 'express'
-import { createUsuario, getUsuarios } from '../controllers/user.controller.js'
-
-const router = express.Router()
-
-router.post('/crearUsuario', createUsuario)
-router.get('/listaUsuarios', getUsuarios)
-
-export default router
+c
