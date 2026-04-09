dd46afdb81c2fb5532bf0384388361342f6c50c3
Edwin Cedeno <edwin.e.cedeno@gmail.com>
2026-04-09 15:14:15 -0500

    fix 3

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const PORT = process.env.PORT || 8080;
 //app.listen(3000, () => {
  console.log('Server running on http://localhost:3000')
 //});
-
-app.listen (PORT, () => {
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.get ('/', (req,res) => {
   res.send ('Api Funcionando...');
-})
+});
 
 
 /*import pkg from 'pg';
